12a3ed8a5e04ff9bd2fc2203930811b7ce77cd67
Lena Foon <lena.foon@gmail.com>
2023-08-29 14:10:56 +0200

    done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -5,38 +5,33 @@ const Celebrity = require("../models/Celebrity.model")
 
 // all your routes here
 
-router.get("/create", (req, res) => {
+router.get("/celebrities/create", (req, res) => {
     res.render("celebrities/new-celebrity");
-});
+  });
+  
 
-router.post("/create", (req, res) => {
+  router.post("/celebrities/create", async (req, res) => {
+    try {
+      const { name, occupation, catchPhrase } = req.body;
 
-    const { name, occupation, catchPhrase} = req.body;
+    await Celebrity.create({ name, occupation, catchPhrase });
+      res.redirect("/celebrities");
+    } 
+    catch (error) {
+      console.error(error);
+      res.render("celebrities/new-celebrity", { errorMessage: "Error adding celebrity." });
+    }
+  });
+  
 
-    Celebrity.create({name, occupation, catchPhrase});
-
-    newCelebrity
-    .save()
-    .then(() => {
-        res.redirect("/celebrities")
-        })
-    .catch((err) => {
-        res.render("celebrities/new-celebrity");
-    })
-
-});
-
-
-router.get("/celebrities", (req, res) => {
-    Celebrity.find()
-    .then(() => {
-        res.render("celebrities/celebrities", {celebrities})
-    })
-    .catch((err) => {
-    res.render("celebrities/celebrities");
-})
-    
-
-})
-
+  router.get("/celebrities", async (req, res) => {
+    try {
+      const celebrities = await Celebrity.find();
+      res.render("celebrities/celebrities", { celebrities });
+    } 
+    catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -40,53 +40,94 @@ router.get("/", (req, res) => {
 });
 
 
-router.get("/:id", (req, res, next) => {
-    const movieId = req.params.id;
-  
-    Movie.findById(movieId)
-      .populate("cast")
-
-      .then((movie) => {
-        res.render("movies/movie-details", { movie });
-      })
-
-      .catch((error) => {
-        console.log("Error retrieving movie details:", error);
-        next(error);
-      });
-  });
+router.get('/movies/create', async (req, res) => {
+  try {
+    const celebrities = await Celebrity.find();
+    res.render('movies/new-movie', { celebrities });
+  } 
+  catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 
-  router.post("/:id/delete", (req, res, next) => {
-    const movieId = req.params.id;
-  
-    Movie.findByIdAndRemove(movieId)
-      .then(() => {
-        res.redirect("/movies");
-      })
-
-      .catch((error) => {
-        console.log("Error deleting movie:", error);
-        next(error);
-      });
-  });
+router.post('/movies/create', async (req, res) => {
+  try {
+    await Movie.create(req.body);
+    res.redirect('/movies');
+  } 
+  catch (error) {
+    console.error(error);
+    res.render('movies/new-movie', { error });
+  }
+});
 
 
-  router.get("/:id/edit", (req, res, next) => {
-    const movieId = req.params.id;
-  
-    Promise.all([
-      Movie.findById(movieId).populate("cast"),
-      Celebrity.find(),
-    ])
-      .then(([movie, celebrities]) => {
-        res.render("movies/edit-movie", { movie, celebrities });
-      })
-      
-      .catch((error) => {
-        console.log("Error editing movie:", error);
-        next(error);
-      });
-  });
+
+router.get('/movies', async (req, res) => {
+  try {
+    const movies = await Movie.find();
+    res.render('movies/movies', { movies });
+  } 
+  catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
+router.get('/movies/:id', async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id).populate('cast');
+    res.render('movies/movie-details', { movie });
+  } 
+  catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
+router.post('/movies/:id/delete', async (req, res) => {
+  try {
+
+  await Movie.findByIdAndRemove(req.params.id);
+    res.redirect('/movies');
+  } 
+  catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
+router.get('/movies/:id/edit', async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id).populate('cast');
+    const celebrities = await Celebrity.find();
+    res.render('movies/edit-movie', { movie, celebrities });
+  } 
+  catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+router.post('/movies/:id/edit', async (req, res) => {
+  try {
+    
+  await Movie.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect(`/movies/${req.params.id}`);
+  } 
+  catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 module.exports = router;
